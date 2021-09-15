2d6bb4a4b70a2f9586cd0a4648d66be9dd9ca4b9
Donovan Pearce <dwpearce@princeton.edu>
2021-09-15 17:04:23 -0400

    First attempt at arg parsing

---
+++ reg.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# reg.py
+# Authors: Donovan Pearce and Annette Lee
+#-----------------------------------------------------------------------
+
+import argparse
+
+#-----------------------------------------------------------------------
+
+def main():
+
+	parser = argparse.ArgumentParser(description=
+		"Registrar application: show overviews of classes")
+	parser.add_argument('-d', 'dept', type=str, nargs='?', 
+		dest='dept', action='store', default='', 
+		help='show only those classes whose department contains dept')
+
+	parser.add_argument('-n', 'num', type=str, nargs='?', 
+		dest='num', action='store', default='', 
+		help='show only those classes whose course number contains num')
+
+	parser.add_argument('-a', 'area', type=str, nargs='?', 
+		dest='area', action='store', default='', 
+		help='show only those classes whose distrib area contains area')
+
+	titleHelperStr = 'show only those classes whose course title ' + 
+		'contains title'
+
+	parser.add_argument('-t', 'title', type=str, nargs='?', 
+		dest='title', action='store', default='', 
+		help=titleHelperStr)
+
+	if (dept != '')
+		print 'title: ' + dept
+	if (num != '')
+		print 'num: ' + num
+	if (area != '')
+		print 'area' + area
+	if (title != '')
+		print 'title' + title
+
+
+#-----------------------------------------------------------------------
+
+if __name__ == '__main__':
+	main()
