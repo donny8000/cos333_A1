13b685d5215b00a17d93fa133de3da4a14f0baad
Donovan Pearce <dwpearce@princeton.edu>
2021-09-15 17:31:02 -0400

    Argparsing

--- reg.py
+++ reg.py
@@ -13,15 +13,15 @@ def main():
 
 	parser = argparse.ArgumentParser(description=
 		"Registrar application: show overviews of classes")
-	parser.add_argument('-d', '-dept', type=str, nargs='?', 
+	parser.add_argument('-d', '-dept', type=str, nargs=1, 
 		dest='dept', action='store', default='', 
 		help='show only those classes whose department contains dept')
 
-	parser.add_argument('-n', '-num', type=str, nargs='?', 
+	parser.add_argument('-n num', type=str, nargs='?', 
 		dest='num', action='store', default='', 
 		help='show only those classes whose course number contains num')
 
-	parser.add_argument('-a', '-area', type=str, nargs='?', 
+	parser.add_argument('-a', '--area', type=str, nargs='?', 
 		dest='area', action='store', default='', 
 		help='show only those classes whose distrib area contains area')
 
