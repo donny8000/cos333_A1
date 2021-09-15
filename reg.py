eddd12466fdcbae0e8d97758228996bb862307e5
Donovan Pearce <dwpearce@princeton.edu>
2021-09-15 17:38:15 -0400

    Argparsing

--- reg.py
+++ reg.py
@@ -13,35 +13,35 @@ def main():
 
 	parser = argparse.ArgumentParser(description=
 		"Registrar application: show overviews of classes")
-	parser.add_argument('-d', type=str, nargs='?', metavar='dept',
+	parser.add_argument('-d', type=str, nargs=1, metavar='dept',
 		dest='dept', action='store', default='', 
 		help='show only those classes whose department contains dept')
 
-	parser.add_argument('-n', type=str, nargs='?', metavar='num',
+	parser.add_argument('-n', type=str, nargs=1, metavar='num',
 		dest='num', action='store', default='', 
 		help='show only those classes whose course number contains num')
 
-	parser.add_argument('-a', type=str, nargs='?', metavar='area',
+	parser.add_argument('-a', type=str, nargs=1, metavar='area',
 		dest='area', action='store', default='', 
 		help='show only those classes whose distrib area contains area')
 
 	titleHelperStr = 'show only those classes whose course title '
 	titleHelperStr +='contains title'
 
-	parser.add_argument('-t', type=str, nargs='?', metavar='title',
+	parser.add_argument('-t', type=str, nargs=1, metavar='title',
 		dest='title', action='store', default='', 
 		help=titleHelperStr)
 
 	args = parser.parse_args()
 
 	if (args.dept != ''):
-		print('department: ' + args.dept)
+		print('department: ' + args.dept[0])
 	if (args.num != ''):
 		print('num: ' + args.num[0])
 	if (args.area != ''):
-		print('area' + args.area)
+		print('area' + args.area[0])
 	if (args.title != ''):
-		print('title' + args.title)
+		print('title' + args.title[0])
 
 
 #-----------------------------------------------------------------------
