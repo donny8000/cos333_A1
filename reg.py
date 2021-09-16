fa0a4e1f2fb3efe35a4f4d91d15e7a725d08c00e
Donovan Pearce <dwpearce@princeton.edu>
2021-09-15 20:23:04 -0400

    Argparsing

--- reg.py
+++ reg.py
@@ -5,34 +5,12 @@
 # Authors: Donovan Pearce and Annette Lee
 #-----------------------------------------------------------------------
 
-import argparse
+from reg_arg_parser import parse_args
 
 #-----------------------------------------------------------------------
 
 def main():
-
-	parser = argparse.ArgumentParser(description=
-		"Registrar application: show overviews of classes")
-	parser.add_argument('-d', type=str, metavar='dept',
-		dest='dept', action='store', default='', 
-		help='show only those classes whose department contains dept')
-
-	parser.add_argument('-n', type=str, nargs=1, metavar='num',
-		dest='num', action='store', default='', 
-		help='show only those classes whose course number contains num')
-
-	parser.add_argument('-a', type=str, nargs=1, metavar='area',
-		dest='area', action='store', default='', 
-		help='show only those classes whose distrib area contains area')
-
-	titleHelperStr = 'show only those classes whose course title '
-	titleHelperStr +='contains title'
-
-	parser.add_argument('-t', type=str, nargs=1, metavar='title',
-		dest='title', action='store', default='', 
-		help=titleHelperStr)
-
-	args = parser.parse_args()
+	parse_args()
 
 	if (args.dept != ''):
 		print('department: ' + args.dept)
---
+++ reg_arg_parser.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# reg_arg_parser.py
+# Authors: Donovan Pearce and Annette Lee
+#-----------------------------------------------------------------------
+
+import argparse
+
+#-----------------------------------------------------------------------
+
+def parse_args:
+
+	parser = argparse.ArgumentParser(description=
+		"Registrar application: show overviews of classes")
+	parser.add_argument('-d', type=str, metavar='dept',
+		dest='dept', action='store', default='', 
+		help='show only those classes whose department contains dept')
+
+	parser.add_argument('-n', type=str, metavar='num',
+		dest='num', action='store', default='', 
+		help='show only those classes whose course number contains num')
+
+	parser.add_argument('-a', type=str, metavar='area',
+		dest='area', action='store', default='', 
+		help='show only those classes whose distrib area contains area')
+
+	titleHelperStr = 'show only those classes whose course title '
+	titleHelperStr +='contains title'
+
+	parser.add_argument('-t', type=str, metavar='title',
+		dest='title', action='store', default='', 
+		help=titleHelperStr)
+
+	args = parser.parse_args()
