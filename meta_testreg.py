8311f37792f3da08848576e81ae26a170e7f410e
Donovan Pearce <dwpearce@princeton.edu>
2021-09-15 21:28:46 -0400

    Test automation

---
+++ meta_testreg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# meta_testreg.py
+# Authors: Donovan Pearce and Annette Lee
+#-----------------------------------------------------------------------
+
+import os
+import testreg
+
+#-----------------------------------------------------------------------
+
+
+#-----------------------------------------------------------------------
+
+def main():
+	os.system('python testreg.py reg.py > out1 2>&1')
+	os.system('python testreg.py /../u/cos333/Asgt1Solution/ref_reg.pyc > out2 2>&1')
+	os.system('diff -y out1 out2')
+
+#-----------------------------------------------------------------------
+
+if __name__ == '__main__':
+	main()
