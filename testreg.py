4d2ad24bec8d197be048289b76b59edb680cb44f
Donovan Pearce <dwpearce@princeton.edu>
2021-09-18 18:02:45 -0400

    Testing

--- testreg.py
+++ testreg.py
@@ -11,20 +11,39 @@ import os
 #-----------------------------------------------------------------------
 
 def execute_command(args):
+	print ('Args:', args)
 	os.system('python ' + argv[1] + ' ' + args)
 
 
 #-----------------------------------------------------------------------
 
 def main():
-	execute_command('a qr')
-	execute_command('-A qr')
-	execute_command('"-a " qr')
-	execute_command('-a qr st')
-	execute_command('-a')
-	execute_command('-a qr -d')
-	execute_command('-a -d cos')
-	execute_command('-x')
+	# execute_command('a qr')
+	# execute_command('-A qr')
+	# execute_command('"-a " qr')
+	# execute_command('-a qr st')
+	# execute_command('-a')
+	# execute_command('-a qr -d')
+	# execute_command('-a -d cos')
+	# execute_command('-x')
+	execute_command('')
+	execute_command('-d COS')
+	execute_command('-n 333')
+	execute_command('-n b')
+	execute_command('-a Qr')
+	execute_command('-t intro')
+	execute_command('-t science')
+	execute_command('-t C_S')
+	execute_command('-t c%S')
+	execute_command('-d cos -n 3')
+	execute_command('-d cos -a qr -n 2 -t intro')
+	execute_command('-t "Independent Study"')
+	execute_command('-t "Independent Study "')
+	execute_command('-t "Independent Study  "')
+	execute_command('-t " Independent Study"')
+	execute_command('-t "  Independent Study"')
+	execute_command('-t = -c')
+
 
 #-----------------------------------------------------------------------
 
