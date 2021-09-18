8b00615b411074408575e7b9cf231955404c69ed
Donovan Pearce <dwpearce@princeton.edu>
2021-09-18 17:56:04 -0400

    Tabling

--- reg_table.py
+++ reg_table.py
@@ -25,4 +25,4 @@ def print_table(row_list):
 	for row in row_list:
 		row_string = '{:>5} {:>4} {:>6} {:>4} {:}'.format(*row)
 		print(textwrap.fill(row_string, width=72,
-			subsequent_indent="                             "));
+			subsequent_indent="                     "));
