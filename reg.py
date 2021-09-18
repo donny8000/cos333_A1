d275752a5c24663fe3697fcc2e95c77c32c3b7c8
Donovan Pearce <dwpearce@princeton.edu>
2021-09-18 13:59:42 -0400

    Databasing

--- reg.py
+++ reg.py
@@ -6,20 +6,57 @@
 #-----------------------------------------------------------------------
 
 from reg_arg_parser import parse_args
+from reg_wildcard import wildcard
+from sqlit3 import connect
+from sys import stderr, exit 
+from contextlib import closing
 
 #-----------------------------------------------------------------------
 
+DATABASE_URL ='file:reg.sqlite?mode=ro'
+
 def main():
 	args = parse_args()
+	dept_goal = wildcard(args.dept)
+	num_goal = wildcard(args.num)
+	area_goal = wildcard(args.area)
+	title_goal = wildcard(args.title)
 
-	if (args.dept != ''):
-		print('department: ' + args.dept)
-	if (args.num != ''):
-		print('num: ' + args.num)
-	if (args.area != ''):
-		print('area: ' + args.area)
-	if (args.title != ''):
-		print('title: ' + args.title)
+	try: 
+		with connect (DATABASE_URLm uri=True) as connection:
+			cursor = connect.cursor()
+
+			with closing(connection.cursor()) as cursor
+
+			stmt_str = "SELECT classid dept coursenum area title "
+			stmt_str += "FROM classes, coures, crosslistings "
+			stmt_str += "WHERE classes.courseid = courses.courseid "
+			stmt_str += "AND classes.courseid = "
+			stmt_str += "crosslistings.courseid "
+			stmt_str += "AND dept LIKE ? AND coursenum LIKE ? "
+			stmt_str += "AND area LIKE ? AND title LIKE ?"
+			cursor.execute(stmt_str, [dept_goal, num_goal,
+				area_goal, title_goal])
+
+			row = cursor.fetchone()
+			print ('classid:', str(row[0]))
+			print ('dept:', str(row[1]))
+			print ('coursenum:', str(row[2]))
+			print ('area:', str(row[3]))
+			print ('title:', str(row[4]))
+
+	except Exception as ex:
+		print(ex, file=stderr)
+		exit(1)
+
+	# if (dept != ''):
+	# 	print('department: ' + dept)
+	# if (num != ''):
+	# 	print('num: ' + num)
+	# if (area != ''):
+	# 	print('area: ' + area)
+	# if (title != ''):
+	# 	print('title: ' + title)
 
 
 #-----------------------------------------------------------------------
