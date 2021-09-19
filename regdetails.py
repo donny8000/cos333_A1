469ffce7f697571e4214608c8302dd88fc25624b
Donovan Pearce <dwpearce@princeton.edu>
2021-09-19 14:49:53 -0400

    Detail argparsing

--- regdetails.py
+++ regdetails.py
@@ -6,10 +6,82 @@
 #-----------------------------------------------------------------------
 
 from regdetails_arg_parser import parse_args
+from regdetails_table import print_table
+
+#-----------------------------------------------------------------------
+
+DATABASE_URL ='file:reg.sqlite?mode=ro'
 
 def main():
 	args = parse_args()
-	print (args.classid)
+	id_goal = args.classid
+
+
+	try: 
+		with connect (DATABASE_URL, uri=True) as connection:
+			cursor = connection.cursor()
+
+			with closing(connection.cursor()) as cursor:
+
+				stmt_str = "SELECT courseid, days, starttime, endtime, "
+				stmt_str += "bldg, roomnum WHERE classid = ?" 
+
+				cursor.execute(stmt_str, [id_goal])
+
+				class_row = cursor.fetchone()
+
+				if class_row is None:
+					raise ValueError(
+						'no class with classid %d exists' % id_goal)
+
+				course_id_goal = class_row[0]
+
+				stmt_str = "SELECT area, title, descrip, prereqs "
+				stmt_str += "FROM courses WHERE courses.courseid = ?"
+
+				cursor.execute(stmt_str, [course_id_goal])
+
+				course_row = cursor.fetchone()
+
+				stmt_str = "SELECT dept, coursenum from crosslistings "
+				stmt_str += "WHERE crosslistings.courseid = ?"
+
+
+				cursor.execute(stmt_str, [course_id_goal])
+
+				crossliststings_list = [] 
+				row = cursor.fetchone() 
+				while row is not None:
+					crosslistings_list.append(row)
+					row = cursor.fetchone()
+
+				stmt_str = "SELECT profid from courseprofs "
+				stmt_str += "WHERE courseprofs.courseid = ?"
+
+				cursor.execute(stmt_str, [course_id_goal])
+
+				courseprofs_list = [] 
+				row = cursor.fetchone() 
+				while row is not None:
+					courseprofs_list.append(row)
+					row = cursor.fetchone()
+
+
+				prof_list = []
+				for courseprofs_row in courseprofs_list:
+					stmt_str = "SELECT profname from profs "
+					stmt_str += "WHERE profs.profid = ?"
+
+					cursor.execute(stmt_str, [courseprofs_row[0]])
+
+					row = cursor.fetchone()
+					prof_list.append(row[0])
+				exit(0)
+				
+	except Exception as ex:
+		print(ex, file=stderr)
+		exit(1)
+
 
 #-----------------------------------------------------------------------	
 
---
+++ regdetails_table.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# regdetails_table.py
+# Authors: Donovan Pearce and Annette Lee
+#-----------------------------------------------------------------------
+
+from operator import itemgetter
+import textwrap
+
+#-----------------------------------------------------------------------
+
+def sort_crosslisting_rows(row_list):
+	result = sorted(row_list, key=itemgetter(1))
+	result = sorted(result, key=itemgetter(0))
+	return result
+
+def print_table(class_row, course_row, crosslistings_list, prof_list): 
+	crosslistings_list = sort_crosslisting_rows(crosslistings_list)
+	prof_list = sort(prof_list)
+
+	print("Course Id:", class_row[0])
+
+	print()
+
+	print("Days:", class_row[1])
+	print("Start Time:", class_row[2])
+	print("End Time:", class_row[3])
+	print("Building:", class_row[4])
+	print("Room:", class_row[5])
+
+	print()
+
+	for crosslisting_row in crosslistings_list:
+		print("Dept and Number: %s %s" 
+			%(crosslisting_row[0] crosslisting_row[1]))
+
+	print()
+
+	print("Area:", course_row[0])
+
+	print()
+
+	print("Title:", course_row[1])
+
+	print()
+
+	print("Description:", course_row[2])
+
+	print()
+
+	print("Prerequisites:", course_row[3])
+
+	print()
+
+	for prof in prof_list:
+		print("Professor:", prof)
