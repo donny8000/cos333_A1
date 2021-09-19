5772dfa7a4e64d3dd593745bd9bc59f0475c5776
Donovan Pearce <dwpearce@princeton.edu>
2021-09-19 19:43:57 -0400

    Styyle

--- reg_arg_parser.py
+++ reg_arg_parser.py
@@ -11,29 +11,28 @@ import argparse
 
 def parse_args():
 
-	parser = argparse.ArgumentParser(allow_abbrev=False, description=
+    parser = argparse.ArgumentParser(allow_abbrev=False, description=
 		"Registrar application: show overviews of classes")
 
-	parser.add_argument('-d', type=str, metavar='dept',
+    parser.add_argument('-d', type=str, metavar='dept',
 		dest='dept', action='store', default='',
 		help='show only those classes whose department contains dept')
 
-	parser.add_argument('-n', type=str, metavar='num',
+    parser.add_argument('-n', type=str, metavar='num',
 		dest='num', action='store', default='',
 		help='show only those classes whose course number contains num')
 
-	parser.add_argument('-a', type=str, metavar='area',
+    parser.add_argument('-a', type=str, metavar='area',
 		dest='area', action='store', default='',
 		help='show only those classes whose distrib area contains area')
 
-	title_helper_str = 'show only those classes whose course title '
-	title_helper_str +='contains title'
+    title_helper_str = 'show only those classes whose course title '
+    title_helper_str +='contains title'
 
-	parser.add_argument('-t', type=str, metavar='title',
+    parser.add_argument('-t', type=str, metavar='title',
 		dest='title', action='store', default='',
 		help=title_helper_str)
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	return args
-    
+    return args
