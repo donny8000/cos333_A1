8a6630ed132ec8d255cea248abec0ca87c8c75d0
Donovan Pearce <dwpearce@princeton.edu>
2021-09-19 20:05:09 -0400

    Styyle

--- regdetails_arg_parser.py
+++ regdetails_arg_parser.py
@@ -15,7 +15,7 @@ def parse_args():
 		"Registrar application: show details about a class")
 
     parser.add_argument('classid', type=int, metavar='classid',
-		action='store', 
+		action='store',
         help='show only those classes whose department contains dept')
 
     args = parser.parse_args()
--- regdetails_output.py
+++ regdetails_output.py
@@ -11,61 +11,61 @@ import textwrap
 #-----------------------------------------------------------------------
 
 def sort_crosslisting_rows(row_list):
-	result = sorted(row_list, key=itemgetter(1))
-	result = sorted(result, key=itemgetter(0))
-	return result
+    result = sorted(row_list, key=itemgetter(1))
+    result = sorted(result, key=itemgetter(0))
+    return result
 
 def wrap(text):
-	return textwrap.fill(text, width=72)
+    return textwrap.fill(text, width=72)
 
-def print_output(class_row, course_row, crosslistings_list, prof_list): 
-	crosslistings_list = sort_crosslisting_rows(crosslistings_list)
-	prof_list = sorted(prof_list)
+def print_output(class_row, course_row, crosslistings_list, prof_list):
+    crosslistings_list = sort_crosslisting_rows(crosslistings_list)
+    prof_list = sorted(prof_list)
 
-	result = wrap("Course Id: %s" %class_row[0])
-	result += "\n\n"
+    result = wrap("Course Id: %s" %class_row[0])
+    result += "\n\n"
 
-	result += wrap("Days: %s" %class_row[1])
-	result += "\n"
-	result += wrap("Start time: %s" %class_row[2])
-	result += "\n"
-	result += wrap("End time: %s" %class_row[3])
-	result += "\n"
-	result += wrap("Building: %s" %class_row[4])
-	result += "\n"
-	result += wrap("Room: %s" %class_row[5])
+    result += wrap("Days: %s" %class_row[1])
+    result += "\n"
+    result += wrap("Start time: %s" %class_row[2])
+    result += "\n"
+    result += wrap("End time: %s" %class_row[3])
+    result += "\n"
+    result += wrap("Building: %s" %class_row[4])
+    result += "\n"
+    result += wrap("Room: %s" %class_row[5])
 
-	result += "\n\n"
+    result += "\n\n"
 
-	for crosslisting_row in crosslistings_list:
-		result += wrap("Dept and Number: %s %s" 
-			%(crosslisting_row[0], crosslisting_row[1]))
-		result += "\n"
+    for crosslisting_row in crosslistings_list:
+        result += wrap("Dept and Number: %s %s"
+            %(crosslisting_row[0], crosslisting_row[1]))
+        result += "\n"
 
-	result += "\n"
+    result += "\n"
 
-	area_val = course_row[0]
-	if area_val == '':
-		result += "Area: "
-	else:
-		result += wrap("Area: %s" %area_val)
+    area_val = course_row[0]
+    if area_val == '':
+        result += "Area: "
+    else:
+        result += wrap("Area: %s" %area_val)
 
-	result += "\n\n"
+    result += "\n\n"
 
-	result += wrap("Title: %s" %course_row[1])
+    result += wrap("Title: %s" %course_row[1])
 
-	result += "\n\n"
+    result += "\n\n"
 
-	result += wrap("Description: %s" %course_row[2])
+    result += wrap("Description: %s" %course_row[2])
 
-	result += "\n\n"
+    result += "\n\n"
 
-	result += wrap("Prerequisites: %s" %course_row[3])
+    result += wrap("Prerequisites: %s" %course_row[3])
 
-	result += "\n"
+    result += "\n"
 
-	for prof in prof_list:
-		result += "\n"
-		result += wrap("Professor: %s" %prof)
+    for prof in prof_list:
+        result += "\n"
+        result += wrap("Professor: %s" %prof)
 
-	print(result)
+    print(result)
